7eff3cca391fa8b57308fc7a5d540f3c3c3cc690
niujianyu <niujianyu@taou.com>
2023-09-06 20:55:52 +0800

    MOD: modify alfred-pretty-json-in-fehelper to python

--- pattern.py
+++ pattern.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding:utf-8
 
-
-# 获取命令行输入参数
 import sys
+import pyperclip
+from subprocess import Popen, PIPE
+import re
+
 # 获取指定位置的参数
 args = sys.argv
 arg1 = args[1] if len(args) > 1 else "Microsoft Edge"
@@ -12,18 +14,16 @@ arg2 = args[2] if len(args) > 2 else "chrome-extension://feolnkbgcbjmamimpfcnklg
 application = arg1
 jsonUrl = arg2
 
-import pyperclip
-from subprocess import Popen, PIPE
 ## 实现复制当前选中或者复制的
 #script = "set the clipboard to selection"
 #proc = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, universal_newlines=True)
 #out = proc.communicate(script)[0]
 
 out_string = pyperclip.paste()
+re.sub('None(?=(\s*[,}]))', '"None"', out_string)
 print(out_string)
 out_string = str(out_string)
 
-out_string += "//niujianyu_test"
 pyperclip.copy(out_string)
 
 ## 实现 打开tab标签

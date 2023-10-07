712f230fea60b4b40ec6d87c54e9c14e00a1755e
niujianyu <niujianyu@taou.com>
2023-10-07 11:18:08 +0800

    ADD: add list handler for alfred-pretty-json-in-fehelper

--- pattern.py
+++ pattern.py
@@ -19,16 +19,45 @@ jsonUrl = arg2
 #proc = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, universal_newlines=True)
 #out = proc.communicate(script)[0]
 
+# 获取当前剪贴板内容
 out_string = pyperclip.paste()
+
+# 处理常见的Python内属性 如： None False True
 out_string = re.sub('(?<=:)None(?=(\s*[,}]))', '"None"', out_string)
 out_string = re.sub('(?<=:\s)None(?=(\s*[,}]))', '"None"', out_string)
 out_string = re.sub('(?<=:)False(?=(\s*[,}]))', '"False"', out_string)
 out_string = re.sub('(?<=:\s)False(?=(\s*[,}]))', '"False"', out_string)
 out_string = re.sub('(?<=:)True(?=(\s*[,}]))', '"True"', out_string)
 out_string = re.sub('(?<=:\s)True(?=(\s*[,}]))', '"True"', out_string)
+
+# 处理csv数据为数组， 1\n2\n3 变为 [1,2,3]
+is_list = True
+info_list = out_string.split('\n')
+for i in range(len(info_list)):
+    _info = info_list[i]
+    if not _info:
+        continue
+    if _info[0] == '"' and _info[-1] == '"':
+        continue
+    # 字符中包含非预想字符，不进行处理
+    is_not_all_word = re.search(r'[^\w@\.]', _info)
+    if is_not_all_word:
+        is_list = False
+        break
+    # 如果包含非数字字符，考虑是否前后加上"
+    is_not_all_digit = re.search(r'[\D]', _info)
+    if is_not_all_digit:
+        info_list[i] = '"' + _info + '"'
+if is_list:
+    info_list = [_ for _ in info_list if _]
+    list_string = ','.join(info_list)
+    list_string = '[' + list_string + ']'
+    out_string = list_string
+
 print(out_string)
 out_string = str(out_string)
 
+# 复制修改后的内容到剪贴板
 pyperclip.copy(out_string)
 
 ## 实现 打开tab标签
